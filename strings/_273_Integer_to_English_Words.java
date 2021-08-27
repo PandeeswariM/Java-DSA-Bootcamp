1298456efd00c07162af9ecc489e53242e6683fa
PandeeswariM <71081471+PandeeswariM@users.noreply.github.com>
2021-08-27 11:06:04 +0530

    273_Integer_to_English_Words.java added

---
+++ strings/_273_Integer_to_English_Words.java
@@ -0,0 +1,39 @@
+package strings;
+
+public class _273_Integer_to_English_Words {
+  
+	 private final static String[] lesstwenty= {"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen","Twenty"};
+	 private final static String[] tens= {"","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+	 private final static String[] Thousands= {"","Thousand","Million","Billion"};
+	static String numberWords(int num) {
+		if(num==0)
+			return "Zero";
+		int i=0;
+		String words="";
+		while(num>0) {
+			if(num%1000!=0) {
+				words=helper(num%1000)+Thousands[i]+words;
+			}
+			num/=1000;
+			i++;
+		}
+		return words.trim();
+	}
+	static String helper(int num) {
+		if(num==0)
+			return "";
+		else if(num<20)
+			return lesstwenty[num]+" ";
+		else if(num<100)
+			return tens[num/10]+" "+helper(num%10);
+		else
+			return lesstwenty[num/100]+" Hundred "+helper(num%100);
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println(numberWords(1234567));
+	}
+
+}
